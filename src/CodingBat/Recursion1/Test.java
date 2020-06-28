8c8ac3eaaf07a9651f47b72868a4848da48919a8
Bill Li <billli@gmail.com>
2020-06-27 23:33:02 -0400

    coding bat projects

---
+++ src/CodingBat/Recursion1/Test.java
@@ -0,0 +1,11 @@
+package CodingBat.Recursion1;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(bunnyEars.bunnyEars(N));
+    }
+}
